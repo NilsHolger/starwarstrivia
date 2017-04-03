0a2d238c528cb3211c162cf4d08152160108597b
NilsHolger <nilsholger1307@gmail.com>
2017-04-03 21:43:38 +0200

    first commit

---
+++ src/app/modules/movies/movies.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MoviesListComponent } from './components/movies-list/movies-list.component';
+import { MovieComponent } from './components/movie/movie.component';
+import { MoviesService } from './services/movies.service';
+import { MoviesRoutingModule } from './movies-routing.module';
+import { MaterialModule } from '@angular/material';
+import 'hammerjs';
+
+
+@NgModule({
+  imports: [CommonModule, MaterialModule, MoviesRoutingModule],
+  declarations: [MoviesListComponent, MovieComponent],
+  providers: [MoviesService]
+})
+
+export class MoviesModule { }
